ee206fb01eded96e5b834ab2be7c582a145702ee
EmirGaziKopar <1190505016@ogrenci.kirklareli.edu.tr>
2022-04-11 01:43:41 +0300

    1.6

--- app/src/main/java/com/example/memorygame/MainActivity3.kt
+++ app/src/main/java/com/example/memorygame/MainActivity3.kt
@@ -2,10 +2,24 @@ package com.example.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+    }
+
+
+    fun hard(view: View){
+
+    }
+
+    fun medium(view: View){
+
+    }
+
+    fun scores(view: View){
+
     }
 }
